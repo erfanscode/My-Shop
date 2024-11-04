f48efed733bda3fdc8a5392eca0b86dc9fa0202a
erfanscode <codeerfan@gmail.com>
2024-11-05 02:51:15 +0330

    add django jalali for using persian date

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django_jalali.db import models as jmodels
 
-# Create your models here.
+
+class Category(models.Model):
+    # Category model
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+    def __str__(self):
+        return self.name
