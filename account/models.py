4cbb330554adf8848c31593e9cd54519bd5529b4
erfanscode <codeerfan@gmail.com>
2024-11-20 03:06:02 +0330

    create costume user for project

--- account/models.py
+++ account/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+class ShopUserManager(BaseUserManager):
+    # Manager for user handle
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('شماره موبایل الزامی است')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('برای superuser مقدار is_staff=True اجباری است')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('برای superuser مقدار is_superuser=True اجباری است')
+
+        return self.create_user(phone, password, **extra_fields)
+
+
+class ShopUser(AbstractBaseUser, PermissionsMixin):
+    # Costume user for shop
+    phone = models.CharField(max_length=11, unique=True, verbose_name='موبایل')
+    first_name = models.CharField(max_length=50, verbose_name='نام')
+    last_name = models.CharField(max_length=50, verbose_name='نام خانوادگی')
+    address = models.TextField(verbose_name='آدرس')
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = ShopUserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone
+
+
+
--- base/settings.py
+++ base/settings.py
@@ -131,3 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Costume user conf
+AUTH_USER_MODEL = 'account.ShopUser'
