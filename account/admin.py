e3dcecafdfedf27e09d825b0b7f9b9b3d96279d8
erfanscode <codeerfan@gmail.com>
2024-11-20 03:18:11 +0330

    show account tabel in admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from dataclasses import fields
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+
+
+@admin.register(ShopUser)
+class ShopUserAdmin(UserAdmin):
+    ordering = ['phone']
+    list_display = ['phone', 'first_name', 'last_name', 'is_active', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {'fields': ('address',)}
+        ),
+    )
