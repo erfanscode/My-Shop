85c8677ed194edb94dfe7df2b39c3f44e7e2be01
erfanscode <codeerfan@gmail.com>
2025-01-25 12:04:22 +0330

    add verification code feature

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login
+
+from account.models import ShopUser
 from .forms import PhoneVerificationForm
 from cart.common.KaveSms import *
 import random
@@ -23,4 +26,23 @@ def verify_phone(request):
     return render(request, 'forms/verify-phone.html', {'form': form})
 
 def verify_code(request):
-    pass
+    if request.method == "POST":
+        code = request.POST.get('code')
+        if code:
+            verification_code = request.session['verification-code']
+            phone = request.session['phone']
+            if code == verification_code:
+                characters = "QWERT123456789!@#$"
+                user_password = ''.join(random.sample(characters, 8))
+                user = ShopUser.objects.create_user(phone=phone)
+                user.set_password(user_password)
+                user.save()
+                send_data = {'token1': phone, 'token2': user_password}
+                send_sms_with_template(phone, send_data, 'create-account')
+                login(request, user)
+                del request.session['verification-code']
+                del request.session['phone']
+                return redirect("orders:order-create")
+            else:
+                messages.error(request, 'کد وارد شده صحیح نمی باشد')
+    return render(request, 'forms/verify-code.html')
