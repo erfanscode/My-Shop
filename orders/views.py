a5b0aef274f4c0473a7f126c432b369c936319f3
erfanscode <codeerfan@gmail.com>
2025-01-25 11:35:34 +0330

    create view for verify phone number

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import PhoneVerificationForm
+from cart.common.KaveSms import *
+import random
 
-# Create your views here.
+
+def verify_phone(request):
+    if request.user.is_authenticated:
+        return redirect("orders:order-create")
+    if request.method == "POST":
+        form = PhoneVerificationForm(request.POST)
+        if form.is_valid():
+            phone = form.cleaned_data.get('phone')
+            tokens = {'token': ''.join(random.sample('0123456789', 6))}
+            request.session['verification-code'] = tokens['token']
+            request.session['phone'] = phone
+            send_sms_with_template(phone, tokens, 'verification-code')
+            messages.success(request, 'کد تایید با موفقیت ارسال شد')
+            return redirect("orders:verify-code")
+    else:
+        form = PhoneVerificationForm()
+    return render(request, 'forms/verify-phone.html', {'form': form})
+
+def verify_code(request):
+    pass
